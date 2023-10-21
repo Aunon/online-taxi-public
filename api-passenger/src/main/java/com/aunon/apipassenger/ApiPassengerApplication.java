6a198cf990f18607f1f178c2325799593784f7b5
Aunon <2241185730@qq.com>
2023-10-21 17:06:23 +0800

    搭建nacos

--- api-passenger/src/main/java/com/aunon/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/aunon/apipassenger/ApiPassengerApplication.java
@@ -2,6 +2,7 @@ package com.aunon.apipassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPassengerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiPassengerApplication.class);
