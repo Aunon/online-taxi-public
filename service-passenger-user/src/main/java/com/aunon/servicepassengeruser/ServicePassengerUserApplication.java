132c3c15a49c26f06a5769440907cff840776025
Aunon <2241185730@qq.com>
2023-10-22 14:04:05 +0800

    搭建ServicePassengerUser

---
+++ service-passenger-user/src/main/java/com/aunon/servicepassengeruser/ServicePassengerUserApplication.java
@@ -0,0 +1,18 @@
+package com.aunon.servicepassengeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Aunon
+ * @Date: 2023/10/22/14:02
+ * @Description:
+ */
+@SpringBootApplication
+public class ServicePassengerUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerUserApplication.class);
+    }
+}
