6eea6acd93e11e31ed2b0e9c0f87d36fc4f3e45c
Aunon <2241185730@qq.com>
2023-10-22 18:21:59 +0800

    插入用户

--- service-passenger-user/src/main/java/com/aunon/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/aunon/servicepassengeruser/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,13 +27,31 @@ public class UserService {
     @Autowired
     private PassengerUserMapper passengerUserMapper;
 
+    /**
+     * 根据手机号查询用户信息，如果没有则创建
+     * @param passengerPhone
+     * @return
+     */
     public ResponseResult loginOrRegister(String passengerPhone){
 
         System.out.println("service被调用，手机号："+passengerPhone);
 
         Map<String,Object> map = new HashMap<>();
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.size()==0?"无记录":passengerUsers.get(0).getPassengerPhone());
+
+        if(passengerUsers.size()==0){
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte) 0);
+
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+        }
 
 
         return ResponseResult.success();
