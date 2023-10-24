503371f99384cbb063e551de98523690955243f2
Aunon <2241185730@qq.com>
2023-10-24 11:37:02 +0800

    service-price骨架编写完成

---
+++ service-price/src/main/java/com/aunon/serviceprice/ServicePriceApplication.java
@@ -0,0 +1,18 @@
+package com.aunon.serviceprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Aunon
+ * @Date: 2023/10/24/11:23
+ * @Description:
+ */
+@SpringBootApplication
+public class ServicePriceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePriceApplication.class);
+    }
+}
--- service-price/src/main/java/com/aunon/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/aunon/serviceprice/service/ForecastPriceService.java
@@ -1,6 +1,5 @@
-package com.aunon.apipassenger.service;
+package com.aunon.serviceprice.service;
 
-import com.aunon.internalcommon.dto.ForecastPriceDTO;
 import com.aunon.internalcommon.dto.ResponseResult;
 import com.aunon.internalcommon.response.ForecastPriceResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -10,21 +9,13 @@ import org.springframework.stereotype.Service;
  * Created with IntelliJ IDEA.
  *
  * @Author: Aunon
- * @Date: 2023/10/24/10:38
+ * @Date: 2023/10/24/11:28
  * @Description:
  */
 @Service
 @Slf4j
 public class ForecastPriceService {
 
-    /**
-     * 根据出发地和目的地经纬度预估价格
-     * @param depLongitude
-     * @param depLatitude
-     * @param destLongitude
-     * @param destLatitude
-     * @return
-     */
     public ResponseResult forecastPrice(String depLongitude,String depLatitude,String destLongitude,String destLatitude){
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
         forecastPriceResponse.setPrice(12.32);
@@ -34,7 +25,11 @@ public class ForecastPriceService {
         log.info("DestLongitude :"+destLongitude);
         log.info("DestLatitude :"+destLatitude);
 
-        //调用计价服务，计算价格
+        //预估地图服务，查询距离和时长
+
+        //读取计价规则
+
+        //根据距离、时长和计价规则计算价格
 
         return ResponseResult.success(forecastPriceResponse);
     }
