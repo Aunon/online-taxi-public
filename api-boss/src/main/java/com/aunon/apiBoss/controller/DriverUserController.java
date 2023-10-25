ef9b3f2697250a78d193a5d3bd2a5ff945232820
Aunon <2241185730@qq.com>
2023-10-25 23:46:19 +0800

    api-boss调用service-driver-user实现修改司机信息

--- api-boss/src/main/java/com/aunon/apiBoss/controller/DriverUserController.java
+++ api-boss/src/main/java/com/aunon/apiBoss/controller/DriverUserController.java
@@ -5,6 +5,7 @@ import com.aunon.internalCommon.dto.DriverUser;
 import com.aunon.internalCommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,23 @@ public class DriverUserController {
     @Autowired
     private DriverUserService driverUserService;
 
+    /**
+     * 添加司机
+     * @param driverUser
+     * @return
+     */
     @PostMapping("/driver-user")
     public ResponseResult addDriverUser(@RequestBody DriverUser driverUser){
         return driverUserService.addDriverUser(driverUser);
+    }
+
+    /**
+     * 修改司机信息
+     * @param driverUser
+     * @return
+     */
+    @PutMapping("/driver-user")
+    public ResponseResult updateDriverUser(@RequestBody DriverUser driverUser){
+        return driverUserService.updateDriverUser(driverUser);
     }
 }
