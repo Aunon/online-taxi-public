7152e1cb53571c8f452c3d3421edf638b865cf01
Aunon <2241185730@qq.com>
2023-11-07 15:49:55 +0800

    查询城市和车型是否开通业务，是否具有计价规则

--- service-price/src/main/java/com/aunon/serviceprice/controller/PriceRuleController.java
+++ service-price/src/main/java/com/aunon/serviceprice/controller/PriceRuleController.java
@@ -5,12 +5,9 @@ import com.aunon.internalcommon.dto.PriceRule;
 import com.aunon.internalcommon.dto.ResponseResult;
 import com.aunon.serviceprice.service.PriceRuleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -36,4 +33,8 @@ public class PriceRuleController {
         return priceRuleService.edit(priceRule);
     }
 
+    @GetMapping("/if-exists")
+    public ResponseResult<Boolean> ifExists(@RequestBody PriceRule priceRule){
+        return priceRuleService.ifExists(priceRule);
+    }
 }
