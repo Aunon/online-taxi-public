d7fc0b11b75c232ba2d4180177ef91ccbbc058ca
Aunon <2241185730@qq.com>
2023-11-14 15:35:22 +0800

    司机监听及消息推送

--- sse-driver-client-web/src/main/java/com/aunon/ssedriverclientweb/controller/SseController.java
+++ sse-driver-client-web/src/main/java/com/aunon/ssedriverclientweb/controller/SseController.java
@@ -2,26 +2,50 @@ package com.aunon.ssedriverclientweb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-/**
- * Created with IntelliJ IDEA.
- *
- * @Author: Aunon
- * @Date: 2023/11/14/13:13
- * @Description:
- */
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 public class SseController {
 
+    public static Map<String, SseEmitter> sseEmitterMap = new HashMap<>();
+
+    /**
+     * 建立连接
+     * @param driverId
+     * @return
+     */
     @GetMapping("/connect/{driverId}")
     public SseEmitter connect(@PathVariable String driverId){
         System.out.println("司机ID:"+driverId);
         SseEmitter sseEmitter = new SseEmitter(0l);
 
+        sseEmitterMap.put(driverId, sseEmitter);
+
         return sseEmitter;
+    }
+
+    /**
+     * 发送消息
+     * @param driverId 消息接收者
+     * @param content 消息内容
+     * @return
+     */
+    @GetMapping("/push")
+    public String push(@RequestParam String driverId, @RequestParam String content){
+
+        try {
+            sseEmitterMap.get(driverId).send(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "给用户："+driverId+",发送了消息："+content;
     }
 }
