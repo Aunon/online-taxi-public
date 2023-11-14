9e4d1c76b9745eda3e1342a38e8708bdb73bbd4c
Aunon <2241185730@qq.com>
2023-11-14 13:03:52 +0800

    模拟司机客户端开发

---
+++ sse-driver-client-web/src/main/java/com/aunon/ssedriverclientweb/SseDriverClientWebApplication.java
@@ -0,0 +1,18 @@
+package com.aunon.ssedriverclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Aunon
+ * @Date: 2023/11/14/12:50
+ * @Description:
+ */
+@SpringBootApplication
+public class SseDriverClientWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SseDriverClientWebApplication.class,args);
+    }
+}
