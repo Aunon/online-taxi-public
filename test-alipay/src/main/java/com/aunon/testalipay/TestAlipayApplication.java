46e69ab6b4064ff90f5bd3e69a5dd9c7de38ab50
Aunon <2241185730@qq.com>
2023-11-16 15:45:27 +0800

    支付宝付款及回调完成

---
+++ test-alipay/src/main/java/com/aunon/testalipay/TestAlipayApplication.java
@@ -0,0 +1,27 @@
+package com.aunon.testalipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Aunon
+ * @Date: 2023/11/16/12:41
+ * @Description:
+ */
+@SpringBootApplication
+@RestController
+public class TestAlipayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestAlipayApplication.class);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("支付宝回调啦");
+        return "外网穿透测试";
+    }
+}
